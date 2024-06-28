30244044c1a7bbf8e567f80944c737ced5b0b61a
unbany <130903252+soneblee@users.noreply.github.com>
2024-06-28 14:51:32 +0900

    25083번 해결

---
+++ 백준/Bronze/Praticing/BOJ25083.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0;
+}
+
+// " 을 출력하려면 \"
+// \ 을 출력하려면 \\ 
