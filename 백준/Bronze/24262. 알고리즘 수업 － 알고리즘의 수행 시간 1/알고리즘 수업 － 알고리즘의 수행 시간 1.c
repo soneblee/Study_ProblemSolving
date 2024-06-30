553dbd50089b8ab5fd24ec49a4097c670f98b255
unbany <130903252+soneblee@users.noreply.github.com>
2024-06-30 09:54:16 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.c
@@ -0,0 +1,21 @@
+// 시간 복잡도에 대한 문제 - 다항식을 구하라고 했으나 MenOfPassion 알고리즘은 최고차항이 항상 0이다.
+
+/*
+MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1 // return 값에 의하면 항상 실행횟수가 0임
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int n;
+
+    scanf("%d", &n);
+    printf("1\n0");
+
+    return 0;
+
+}
