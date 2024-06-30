8a8f19be1a7b7c68b82dcd8b57dc173305a005e5
unbany <130903252+soneblee@users.noreply.github.com>
2024-06-30 10:28:25 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n; // SciComLove 를 출력헐 횟수 (1 or 2)
+
+    scanf("%d", &n);
+
+    for (int i=1; i<=n; i++)
+        printf("SciComLove\n");
+
+    return 0;
+}
