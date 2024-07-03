c93ca0687d44c50b3d569630bf4d52490595281d
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-03 09:52:29 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |\n");
+
+    return 0;
+}
