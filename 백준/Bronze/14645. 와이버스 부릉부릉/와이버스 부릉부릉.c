a97dd33aaf25ebc0aef74ead03b5193b3e8f82c6
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-07 17:07:41 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n, k, a, b;
+
+    scanf("%d %d", &n, &k);
+
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &a, &b);
+    }
+
+    printf("비와이");
+
+    return 0;
+}
