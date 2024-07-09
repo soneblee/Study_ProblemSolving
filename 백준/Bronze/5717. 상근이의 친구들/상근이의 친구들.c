dbd2dfdcffed5c818adbb999b81c05f72568911c
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-09 22:58:42 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int m, f;
+
+    while(1){
+        scanf("%d %d", &m, &f);
+        
+        if(m == 0 && f == 0)
+            break;
+        
+        printf("%d\n", m+f);
+    }
+
+    return 0;
+}
