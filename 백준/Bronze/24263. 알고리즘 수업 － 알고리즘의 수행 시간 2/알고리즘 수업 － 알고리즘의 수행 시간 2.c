28b432b5d9bb590942bb2ce006b1f4fd50e7689d
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-10 15:24:52 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.c
@@ -0,0 +1,21 @@
+/*
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+첫째줄: 코드1의 수행 횟수 출력
+둘째줄: 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수 출력
+단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력
+}
+
+*/
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d\n1", n);
+
+    return 0;
+}
