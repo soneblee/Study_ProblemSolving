aa0e214e7e1ed0364d10f97a1c7012574f9d9443
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-10 11:42:50 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int k, n, m;
+    scanf("%d %d %d", &k, &n, &m);
+
+    if(k*n > m)
+        printf("%d", k*n - m);
+    else
+        printf("0");
+
+    return 0;
+}
