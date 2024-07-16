edb12ed73cf5b7aaccd3df6f26a0feb6da68ff83
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-16 18:21:04 +0900

    [Bronze IV] Title: 전자레인지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c, d, e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    if(a<0)
+        printf("%d", -1*a*c + d + b*e);
+    else if(a>0)
+        printf("%d", (b-a)*e);
+
+    return 0;
+}
