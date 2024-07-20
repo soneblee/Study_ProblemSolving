17562ecaedf2338e964f3539229f7519996ee497
unbany <130903252+soneblee@users.noreply.github.com>
2024-07-20 14:27:15 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b); 
+
+    if(a/2 < b)
+        printf("%d", a/2);
+    else
+        printf("%d", b);
+
+    return 0;
+}
