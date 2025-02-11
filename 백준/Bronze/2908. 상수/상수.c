9765a74c94e8e4f7bae7d1f6c9bad63ece2e33f0
Eunbin Lee <130903252+soneblee@users.noreply.github.com>
2025-02-11 16:36:59 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
-int main(){
-    int n, m, n2, m2;
-    int sum = 0;
-    scanf("%d %d", &n, &m);
+int main() {
+	char a[4], b[4];
 
-    int a = n % 10;
-    int b = (n / 10) % 10;
-    int c = n / 100;
-    int a2 = m % 10;
-    int b2 = (m / 10) % 10;
-    int c2 = m / 100;
-    n2 = a * 100 + b * 10 + c;
-    m2 = a2 * 100 + b2 * 10 + c2;
+	scanf("%s %s", a, b);
 
-    if(n2 > m2)
-        printf("%d", n2);
-    else
-        printf("%d", m2);
+	for (int i = 2; i >= 0; i--) {
+		if (a[i] > b[i]) {
+			printf("%c%c%c\n", a[2], a[1], a[0]);
+			break;
+		}
 
-    return 0;
+		else if (a[i] < b[i]) {
+			printf("%c%c%c\n", b[2], b[1], b[0]);
+			break;
+		}
+	}
+	return 0;
 }
